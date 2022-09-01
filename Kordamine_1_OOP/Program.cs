2d70a0e62c57436e57087ad0c29a03c948db6f08
AleksandrAleksejev <sashaaleksejev813577@gmail.com>
2022-09-01 19:13:44 +0300

    Finnaly

--- Kordamine_1_OOP/Program.cs
+++ Kordamine_1_OOP/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Kordamine_1_OOP;
-// See https://aka.ms/new-console-template for more information
+//See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 //Koduloom kass = new Koduloom("Murka", "punane", 'I', 5.5, 7, "Elus");
 //kass.print_info();
@@ -9,5 +9,13 @@
 //kassKloon.muuda_Kaal(2.3);
 //kassKloon.muuda_Sugu('I');
 //kassKloon.print_info();
-Rat rat = new Rat(" Dambo", "Ratix", "punane", (double)Koduloom.sugu.Isane, (Koduloom.sugu)0.5, 1, "Elus");
-rat.print_info();
+//Rat rat = new Rat(" Dambo", "Ratix", "punane", (double)Koduloom.sugu.Isane, (Koduloom.sugu)0.5, 1, "Elus");
+//rat.print_info();
+Tootaja tootaja = new Tootaja("THK", "õpetaja", "Marina", 1989, Isik.Sugu.tudruk, 200, 600, 1400);
+Opilane Opilane = new Opilane("THK", "opilane", "Aleksandr", 2005, Isik.Sugu.poiss, 100, 300, 700);
+Kutsekooliopilane Kutsekooliopilane = new Kutsekooliopilane("THK", "kutsekooliopilane", "Dany", 2006, Isik.Sugu.poiss, 100, 300, 700);
+tootaja.print_Info();
+Opilane.print_Info();
+Kutsekooliopilane.print_Info();
+
+
